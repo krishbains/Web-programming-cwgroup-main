b8dbff6fe292fb417aecf5e5f0e6df77d20c56fc
VinuAnbu <120427696+VinuAnbu@users.noreply.github.com>
2025-01-16 22:50:20 +0000

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,24 +2,31 @@
     <main class="container pt-4">
         <div>
             <router-link
-                class=""
+                class="router-link"
                 :to="{name: 'Main Page'}"
             >
                 Main Page
             </router-link>
             |
             <router-link
-                class=""
+                class="router-link"
                 :to="{name: 'Other Page'}"
             >
                 Other Page
             </router-link>
             |
             <router-link
-                class=""
-                :to="{name: 'Profile page'}"
+                class="router-link"
+                :to="{name: 'Profile Page'}"
             >
                 Profile Page
+            </router-link>
+            |
+            <router-link
+                class="router-link"
+                :to="{name: 'All Hobbies'}"
+            >
+                All Hobbies
             </router-link>
         </div>
         <RouterView class="flex-shrink-0" />
@@ -33,8 +40,18 @@ import { RouterView } from "vue-router";
 export default defineComponent({
     components: { RouterView },
 });
-
 </script>
 
 <style scoped>
+.router-link {
+    text-decoration: none;
+    color: blue;
+    font-weight: bold;
+    padding: 0 5px;
+    transition: color 0.3s ease;
+}
+
+.router-link:hover {
+    color: purple;
+}
 </style>
